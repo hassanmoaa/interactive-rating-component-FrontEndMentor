ea0ef69982e3baf5d9979f245dd6af225411d4d3
hassanmoaa <hassanmoataz500@outlook.com>
2024-01-30 22:30:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const numberElBtn = document.getElementById("number-btn")
+const numberElBtn = document.querySelectorAll("rating__card__buttons__number-btn")
+
 
 numberElBtn.addEventListener("click", function() {
+    forEach()
     
 })

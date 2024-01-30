aa2b13895eb8343848e897bc8a9b974c46636405
hassanmoaa <hassanmoataz500@outlook.com>
2024-01-30 23:18:22 +0200

    updated

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const numberElBtn = document.querySelectorAll(".rating__card__buttons__number-btn");
+const submitBtn = document.querySelector(".submit-btn")
+const thankYouPage = document.querySelector(".container__thank-you-card")
+let rating = 0;
+
 
 // add a click event for each rating number button
 numberElBtn.forEach(btn => {
@@ -6,3 +10,14 @@ numberElBtn.forEach(btn => {
         console.log('hello world');
     });
 })
+
+submitBtn.addEventListener("click", function() {
+    
+    if (submitBtn === "none") {
+        thankYouPage.style.display = "flex"
+    } else {
+        thankYouPage.style.display ="none"
+    }
+
+})
+
